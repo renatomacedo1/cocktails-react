f6efddbce7f6826025761e81fe609918510aef2e
Renato Macedo <renato_macedo_1@hotmail.com>
2022-09-09 15:39:42 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+        exercitationem doloribus fugit sequi quidem itaque, ut, nam veritatis
+        expedita harum quas nulla incidunt animi quo sed vel officia minus?
+        Quas.
+      </p>
+    </section>
+  );
+};
+
+export default About;
